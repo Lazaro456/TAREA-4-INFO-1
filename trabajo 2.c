aa79e55efcf164650e17ffdcae3f68c27bf0897f
Lazaro456 <lazakatz@gmail.com>
2025-06-08 16:31:59 -0300

    Add files via upload

---
+++ trabajo 2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+	
+	int main() {
+		int calificacion;
+		
+		printf("Ingrese la calificación: ");
+		scanf("%d", &calificacion);
+		
+		if (calificacion >= 90) {
+			printf("Calificación: A");
+		} else if (calificacion >= 80) {
+			printf("Calificación: B");
+		} else if (calificacion >= 70) {
+			printf("Calificación: C");
+		} else if (calificacion >= 60) {
+			printf("Calificación: D");
+		} else {
+			printf("Calificación: F");
+		}
+		
+		return 0;
+	}
+
